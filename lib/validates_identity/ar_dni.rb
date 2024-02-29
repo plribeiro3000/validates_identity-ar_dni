ec89625803835ed4e5b0e4b16cca478792f64559
Paulo Ribeiro <plribeiro3000@gmail.com>
2024-02-29 19:02:35 -0300

    feat(*): Initial Commit

---
+++ lib/validates_identity/ar_dni.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ar_dni/version"
+
+module ValidatesIdentity
+  module ArDni
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/validates_identity/ar_dni_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ValidatesIdentity::ArDni do
+  it "has a version number" do
+    expect(ValidatesIdentity::ArDni::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
